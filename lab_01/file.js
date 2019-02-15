b3a6c3e0ba3a1350b72a6a8ef00344820fb9076c
Lena <helly.luchina@gmail.com>
2019-02-15 18:23:27 +0300

    array of arrays, range bug found

--- lab_01/file.js
+++ lab_01/file.js
@@ -1,55 +1,60 @@
 const handleFile = (e) => {
-    //document.querySelector('.input-file-trigger').style.background = 'rgb(50, 124, 184)'
+    //fires when file loaded, gets file content as text, calls main process
+    const file = e.target.files[0]
+    if (file) {
+        document.querySelector('.file-return').textContent = `Selected file: ${file.name}`
+        if (window.FileReader) {
+            const reader = new FileReader()
+            reader.readAsText(file)
+            reader.onloadend = () => {
+                console.log('loaded')
+                const allText = reader.result
+                mainProcess(allText);
+            }
+        } else {
+            console.log('filereader is not supported')
+            alert('filereader is not supported')
+        }
+    }
+    else {document.querySelector('.file-return').textContent = `file is not selected`}
+}
+
+function mainProcess(allText) {
+    //clear canvas and output fields
     clearCanvas()
     document.querySelector('p.root').textContent = ''
     document.querySelector('#output').textContent = ''
 
-
-    const file = e.target.files[0]
-    document.querySelector('.file-return').textContent = `Selected file: ${file.name}`
-    if (window.FileReader) {
-        const reader = new FileReader()
-        reader.readAsText(file)
-        reader.onloadend = () => {
-            console.log('loaded')
-            const allText = reader.result
-            clearCanvas()
-            mainProcess(allText);
-        }
-    } else {
-        console.log('filereader is not supported')
-    }
-}
-
-function mainProcess(allText) {
     try {
         let values = processData(allText);
         let xValues = values.x;
         let yValues = values.y;
-        console.log(xValues);
-        console.log(yValues);
-        let min = Math.abs(xValues[0])
-        let max = Math.abs(xValues[xValues.length - 1])
-        let edgeX = min > max ? min : max
+        console.log(xValues, yValues)
 
-        min = Math.abs(yValues[0])
-        max = Math.abs(yValues[yValues.length - 1])
-        let edgeY = min > max ? min : max
-
-        const k = draw(xValues, yValues, edgeX, edgeY);
+        //draw the initial function, get scale koef back
+        let k = getScaleKoef(xValues, yValues)
+        k = 17
+        draw(xValues, yValues, k);
         document.querySelector('#input').style.display = 'block';
 
         document.querySelector('button.root').addEventListener('click', (e) => {
-            console.log('click')
-            const x = findRoot(yValues, xValues)
-            const answer = `~f(${x}) = 0`
-            console.log(answer)
-            document.querySelector('p.root').textContent = answer
+            try {
+                console.log('click')
+                const x = findRoot(yValues, xValues)
+                const answer = `~f(${x.toFixed(5)}) = 0`
+                console.log(answer)
+                document.querySelector('p.root').textContent = answer
+            }
+            catch(e) {
+                alert(e.message)
+            }
         })
 
         document.querySelector('form#xn-input').addEventListener('submit', (e) => {
             clearCanvas()
-            draw(xValues, yValues, edgeX, edgeY);
+            draw(xValues, yValues, k);
+            document.querySelector('p.root').textContent = ''
+            document.querySelector('#output').textContent = ''
             e.preventDefault();
             x = e.target.elements.x.value;
             n = e.target.elements.n.value;
@@ -59,56 +64,55 @@ function mainProcess(allText) {
                 xn = validInput(x, n);
                 x = xn.x;
                 n = xn.n;
-                console.log(`x = ${x},  n = ${n}`);
                 const index = findXIndex(x, xValues);
-                console.log(`index = ${index}`);
                 values = getRange(n, xValues, yValues, index);
                 const xRange = values.x;
+                console.log('xRange')
+                console.log(xRange)
                 let yRange = values.y;
-                console.log(xRange);
-                console.log(yRange);
                 const koefs = getKoefs(xRange, yRange);
-                console.log('koefs');
-                console.log(koefs);
+                console.log('koefs')
+                console.log(koefs)
                 const y = calculate(x, koefs, xRange);
                 printy(x, y)
                 drawByFunc(xValues, calculate, koefs, xRange, k)
-                
-  
             } catch (e) {
                 console.log(e)
-                //alert(e.message);
+                alert(e.message);
             }
         });
     } catch (e) {
         console.log(e)
-        //alert(e.message);
+        alert(e.message);
     }
 }
 
 const findRoot = (xValues, yValues) => {
 
-    console.log(xValues, yValues)
+    //console.log(xValues, yValues)
     const xValuesCopy = xValues.slice()
     const yValuesCopy = yValues.slice()
 
     koefs = getKoefs(xValuesCopy, yValuesCopy)
-    return calculate(0, koefs, xValues)
-
-    // console.log('finding root')
-    // return 1
+    console.log(koefs)
+    const x = calculate(0, koefs, xValues)
+    console.log(`x = ${x}`)
+    if (isNaN(x)) {throw Error("can't find root")}
+    return x
 }
 
 const printy = (x, y) => {
-    const output = `f(${x}) = ${y}`
-    document.querySelector('#output').textContent = output
+    if (x && y) {
+        const output = `f(${x}) = ${y}`
+        document.querySelector('#output').textContent = output
+    }
 }
 
 function processData(allText) {
     let xValues = []
     let yValues = []
+    let values = []
     const elements = allText.split('\n')
-    elements.sort()
     elements.forEach((item) => {
         item = item.split(',')
         const x = item[0]
@@ -116,9 +120,19 @@ function processData(allText) {
         if (isNaN(x) || isNaN(y)) {
             throw Error('Invalid file data')
         }
-        xValues.push(parseFloat(item[0]))
-        yValues.push(parseFloat(item[1]))
+        values.push([parseFloat(item[0]), parseFloat(item[1])])
     })
+    values.sort( (a, b) => {
+        if (a[0] > b [0]) return 1
+        if (a[0] < b[0]) return -1
+        return 0
+    })
+
+    values.forEach((item) => {
+        xValues.push(item[0])
+        yValues.push(item[1])
+    }) 
+    console.log(values)
     return {
         x: xValues,
         y: yValues
@@ -149,6 +163,7 @@ const findXIndex = (x, xValues) => {
     // less than any value
     if (x < xValues[0]) {
         console.log('extrapolation')
+        alert('extropolation')
         //returning first index - 1 = -1
         return -1
     }
@@ -165,6 +180,7 @@ const findXIndex = (x, xValues) => {
     }
     // else x is greater than any value, returning last index + 1
     console.log('extrapolation')
+    alert('extropolation')
     return length
     //extrapolation is just by printing it
 }
@@ -236,19 +252,37 @@ const scaleY = (height, k, y) => {
     return height/2 - k * y
 }
 
-const draw = (xValues, yValues, edgeX, edgeY) => {
+const getScaleKoef = (xValues, yValues) => {
+    const canvas = document.querySelector('#Graph')
+    const width = canvas.width
+    const height = canvas.height
+
+    //finding edged values for scale koefficient
+    let min = Math.abs(xValues[0])
+    let max = Math.abs(xValues[xValues.length - 1])
+    let edgeX = min > max ? min : max
+
+    min = Math.abs(yValues[0])
+    max = Math.abs(yValues[yValues.length - 1])
+    let edgeY = min > max ? min : max
+
+    let absDivX = Math.abs((width - 25) / 2 / edgeX)
+    absDivX = Math.floor(absDivX)
+
+    let absDivY = Math.abs((height - 25) / 2 / edgeY)
+    absDivY = Math.floor(absDivY)
+
+    const k = absDivX < absDivY ? absDivX : absDivY
+
+    return k
+}
+
+const draw = (xValues, yValues, k) => {
     console.log('drawing')
     var canvas = document.querySelector('#Graph')
     if (canvas.getContext) {
         const width = canvas.width
         const height = canvas.height
-        let absDivX = Math.abs((width - 25) / 2 / edgeX)
-        absDivX = Math.floor(absDivX)
-
-        let absDivY = Math.abs((height - 25) / 2 / edgeY)
-        absDivY = Math.floor(absDivY)
-
-        const k = absDivX < absDivY ? absDivX : absDivY
 
         var ctx = canvas.getContext('2d')
 
@@ -258,7 +292,9 @@ const draw = (xValues, yValues, edgeX, edgeY) => {
         for (let i = 1; i < xValues.length; i++) {
             ctx.lineTo(scaleX(width, k, xValues[i]), scaleY(height, k, yValues[i]))
             ctx.stroke()
+            //debugger
         }
+        // ctx.stroke()
         return k
     }    
 }
