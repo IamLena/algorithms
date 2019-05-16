1daf6b9cf3b6528382f2789fa242c92ac1c3f5c6
Lena <helly.luchina@gmail.com>
2019-05-17 00:36:18 +0300

    form

--- lab_06/solve.js
+++ lab_06/solve.js
@@ -1,71 +1,91 @@
-const table = document.querySelector('#tableBody')
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    document.querySelector('#tableBody').innerHTML = ''
+    const a0 = convertToFloat(e.target.elements.a0.value)
+    const a1 = convertToFloat(e.target.elements.a1.value)
+    const a2 = convertToFloat(e.target.elements.a2.value)
+    const xb = convertToFloat(e.target.elements.xb.value)
+    const xe = convertToFloat(e.target.elements.xe.value)
+    const step = convertToFloat(e.target.elements.step.value)
 
-const a0 = 1
-const a1 = 2
-const a2 = 3
-const xb = -3
-const xe = 3
-const step = 0.5
-const length = (xe - xb) / step
+    if (a0 && a1 && a2 && xb && xe && step) {
+        if (xb < xe && step > 0 && a1 != 0) {
+            runProg(a0, a1, a2, xb, xe, step)
+            return
+        }
+    }
+    alert('invalid input')
+})
 
-function func(x) {
-    return a0 * x / (a1 + a2 * x)
+function convertToFloat(x) {
+    if (isNaN(x) || x == '') {
+        return
+    }
+    else return parseFloat(x)
 }
 
-function funcDif(x) {
-    return a0 * a1 / Math.pow(a1 + a2 * x, 2)
-}
-
-for (let x = xb; x <= xe; x += step) {
-    let y = '', d1 = '', d2 = '', d3 = '', d4 = '', d5 = '', d6 =''
-    let precision
-    y = `${func(x).toFixed(5)}`
-    d6 = `${funcDif(x).toFixed(5)}`
-
-
-    if (x != xe && length >= 2) {
-        d1 = rightSideSub_dif(func, x, step).toFixed(5)
-        precision = calcPrecision(d6, d1)
-        d1 = `${d1} (${precision}%)`
+const runProg = function(a0, a1, a2, xb, xe, step) {
+    const func = function(x) {
+        return a0 * x / (a1 + a2 * x)
+    }
+    const funcDif = function(x) {
+        return a0 * a1 / Math.pow(a1 + a2 * x, 2)
     }
 
+    const length = (xe - xb) / step
 
-    if (x == xb && length >= 3) {
-        d2 = leftEdge_dif(func, x, step).toFixed(5)
-        precision = calcPrecision(d6, d2)
-        d2 = `${d2} (${precision}%)`
-    }
-    if (x == xe && length >= 3) {
-        d2 = rightEdge_dif(func, x, step).toFixed(5)
-        precision = calcPrecision(d6, d2)
-        d2 = `${d2} (${precision}%)`
-    }
-
-
-    if (x != xb && x != xe && length >= 3) {
-        d3 = centalSub_dif(func, x, step).toFixed(5)
-        precision = calcPrecision(d6, d3)
-        d3 = `${d3} (${precision}%)`
-    }
-
-
-    if (x != xe && x != xe - step && length >= 3) {
-        d4 = Runge(func, x, step, rightSideSub_dif, 2, 2).toFixed(5)
-        precision = calcPrecision(d6, d4)
-        d4 = `${d4} (${precision}%)`
-    }
-
-
-    if (x != xe && length >= 2) {
-        d5 = alineVars_dif(func, a0, a1, x, step).toFixed(5)
-        precision = calcPrecision(d6, d5)
-        d5 = `${d5} (${precision}%)`
-    }
+    for (let x = xb; x <= xe; x += step) {
+        let y = '', d1 = '', d2 = '', d3 = '', d4 = '', d5 = '', d6 =''
+        let precision
+        y = `${func(x).toFixed(5)}`
+        d6 = `${funcDif(x).toFixed(5)}`
     
-    printLine(x, y, d1, d2, d3, d4, d5, d6)
+    
+        if (x != xe && length >= 2) {
+            d1 = rightSideSub_dif(func, x, step).toFixed(5)
+            precision = calcPrecision(d6, d1)
+            d1 = `${d1} (${precision}%)`
+        }
+    
+    
+        if (x == xb && length >= 3) {
+            d2 = leftEdge_dif(func, x, step).toFixed(5)
+            precision = calcPrecision(d6, d2)
+            d2 = `${d2} (${precision}%)`
+        }
+        if (x == xe && length >= 3) {
+            d2 = rightEdge_dif(func, x, step).toFixed(5)
+            precision = calcPrecision(d6, d2)
+            d2 = `${d2} (${precision}%)`
+        }
+    
+    
+        if (x != xb && x != xe && length >= 3) {
+            d3 = centalSub_dif(func, x, step).toFixed(5)
+            precision = calcPrecision(d6, d3)
+            d3 = `${d3} (${precision}%)`
+        }
+    
+    
+        if (x != xe && x != xe - step && length >= 3) {
+            d4 = Runge(func, x, step, rightSideSub_dif, 2, 2).toFixed(5)
+            precision = calcPrecision(d6, d4)
+            d4 = `${d4} (${precision}%)`
+        }
+    
+    
+        if (x != xe && length >= 2) {
+            d5 = alineVars_dif(func, a0, a1, x, step).toFixed(5)
+            precision = calcPrecision(d6, d5)
+            d5 = `${d5} (${precision}%)`
+        }
+        
+        printLine(x, y, d1, d2, d3, d4, d5, d6)
+    }
 }
 
 function printLine(x, y, d1, d2, d3, d4, d5, d6) {
+    const table = document.querySelector('#tableBody')
     const newLine = document.createElement('tr');
     newLine.innerHTML =`
     <td>${x}</td>
@@ -78,4 +98,3 @@ function printLine(x, y, d1, d2, d3, d4, d5, d6) {
     <td>${d6}</td>`;
     table.appendChild(newLine)
 }
-
