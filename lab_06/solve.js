e7193bdee58034d6d2b182ba458dadd4c53fcc8d
Lena <helly.luchina@gmail.com>
2019-05-23 21:56:54 +0300

    method corrected

--- lab_06/solve.js
+++ lab_06/solve.js
@@ -1,8 +1,8 @@
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
     document.querySelector('#tableBody').innerHTML = ''
-    document.querySelector('#canvas1').getContext('2d').clearRect(0, 0, 500, 400)
-    document.querySelector('#canvas2').getContext('2d').clearRect(0, 0, 500, 400)
+    // document.querySelector('#canvas1').getContext('2d').clearRect(0, 0, 500, 400)
+    // document.querySelector('#canvas2').getContext('2d').clearRect(0, 0, 500, 400)
 
     const a0 = convertToFloat(e.target.elements.a0.value)
     const a1 = convertToFloat(e.target.elements.a1.value)
@@ -71,7 +71,7 @@ const runProg = function(a0, a1, a2, xb, xe, step) {
     
     
         if (x != xe && x != xe - step && length >= 3) {
-            d4 = Runge(func, x, step, rightSideSub_dif, 2, 2).toFixed(5)
+            d4 = Runge(func, x, step, rightSideSub_dif, 2, 1).toFixed(5)
             precision = calcPrecision(d6, d4)
             d4 = `${d4} (${precision}%)`
         }
@@ -93,83 +93,6 @@ const runProg = function(a0, a1, a2, xb, xe, step) {
     const scaleY = (k, y) => {
         return 200 - k * y
     }
-
-    const canvas1 = document.querySelector('#canvas1')
-    if (canvas1.getContext) {
-        var ctx = canvas1.getContext('2d')
-
-        ctx.beginPath();
-        ctx.strokeStyle = 'black'
-        ctx.moveTo(500/2, 0)
-        ctx.lineTo(500/2 - 5, 12)
-        ctx.moveTo(500/2, 0)
-        ctx.lineTo(500/2 + 5, 12)
-        ctx.moveTo(500/2, 0)
-        ctx.lineTo(500/2, 400)
-        ctx.stroke()
-        //y
-        ctx.beginPath();
-        ctx.strokeStyle = 'black'
-        ctx.moveTo(500, 400/2)
-        ctx.lineTo(500-12, 400/2-5)
-        ctx.moveTo(500, 400/2)
-        ctx.lineTo(500-12, 400/2+5)
-        ctx.moveTo(0, 400/2)
-        ctx.lineTo(500, 400/2)
-        ctx.stroke()
-
-        const k = 50
-
-        ctx.beginPath();
-        ctx.strokeStyle = 'red'
-        ctx.stroke()
-        ctx.moveTo(scaleX(xe, k), scaleY(func(xe), k))
-        for (let x = xb; x <= xe; x += 0.2) {
-            console.log(scaleX(x, k), scaleY(func(x), k))
-            ctx.lineTo(scaleX(x, k), scaleY(func(x), k))
-            
-        }
-        ctx.stroke()
-    }
-
-    const canvas2 = document.querySelector('#canvas2')
-    if (canvas2.getContext) {
-        var ctx = canvas2.getContext('2d')
-
-        ctx.beginPath();
-        ctx.strokeStyle = 'black'
-        ctx.moveTo(500/2, 0)
-        ctx.lineTo(500/2 - 5, 12)
-        ctx.moveTo(500/2, 0)
-        ctx.lineTo(500/2 + 5, 12)
-        ctx.moveTo(500/2, 0)
-        ctx.lineTo(500/2, 400)
-        ctx.stroke()
-        //y
-        ctx.beginPath();
-        ctx.strokeStyle = 'black'
-        ctx.moveTo(500, 400/2)
-        ctx.lineTo(500-12, 400/2-5)
-        ctx.moveTo(500, 400/2)
-        ctx.lineTo(500-12, 400/2+5)
-        ctx.moveTo(0, 400/2)
-        ctx.lineTo(500, 400/2)
-        ctx.stroke()
-
-        const k = 50
-
-        ctx.beginPath();
-        ctx.strokeStyle = 'red'
-        ctx.stroke()
-        ctx.moveTo(scaleX(xe, k), scaleY(funcDif(xe), k))
-        for (let x = xb; x <= xe; x += 0.2) {
-            console.log(scaleX(x, k), scaleY(funcDif(x), k))
-            ctx.lineTo(scaleX(x, k), scaleY(funcDif(x), k))
-        }
-        ctx.stroke()
-    }
-
-
 }
 
 function printLine(x, y, d1, d2, d3, d4, d5, d6) {
@@ -187,3 +110,78 @@ function printLine(x, y, d1, d2, d3, d4, d5, d6) {
     table.appendChild(newLine)
 }
 
+
+    // const canvas1 = document.querySelector('#canvas1')
+    // if (canvas1.getContext) {
+    //     var ctx = canvas1.getContext('2d')
+
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = 'black'
+    //     ctx.moveTo(500/2, 0)
+    //     ctx.lineTo(500/2 - 5, 12)
+    //     ctx.moveTo(500/2, 0)
+    //     ctx.lineTo(500/2 + 5, 12)
+    //     ctx.moveTo(500/2, 0)
+    //     ctx.lineTo(500/2, 400)
+    //     ctx.stroke()
+    //     //y
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = 'black'
+    //     ctx.moveTo(500, 400/2)
+    //     ctx.lineTo(500-12, 400/2-5)
+    //     ctx.moveTo(500, 400/2)
+    //     ctx.lineTo(500-12, 400/2+5)
+    //     ctx.moveTo(0, 400/2)
+    //     ctx.lineTo(500, 400/2)
+    //     ctx.stroke()
+
+    //     const k = 50
+
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = 'red'
+    //     ctx.stroke()
+    //     ctx.moveTo(scaleX(xe, k), scaleY(func(xe), k))
+    //     for (let x = xb; x <= xe; x += 0.2) {
+    //         console.log(scaleX(x, k), scaleY(func(x), k))
+    //         ctx.lineTo(scaleX(x, k), scaleY(func(x), k))
+            
+    //     }
+    //     ctx.stroke()
+    // }
+
+    // const canvas2 = document.querySelector('#canvas2')
+    // if (canvas2.getContext) {
+    //     var ctx = canvas2.getContext('2d')
+
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = 'black'
+    //     ctx.moveTo(500/2, 0)
+    //     ctx.lineTo(500/2 - 5, 12)
+    //     ctx.moveTo(500/2, 0)
+    //     ctx.lineTo(500/2 + 5, 12)
+    //     ctx.moveTo(500/2, 0)
+    //     ctx.lineTo(500/2, 400)
+    //     ctx.stroke()
+    //     //y
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = 'black'
+    //     ctx.moveTo(500, 400/2)
+    //     ctx.lineTo(500-12, 400/2-5)
+    //     ctx.moveTo(500, 400/2)
+    //     ctx.lineTo(500-12, 400/2+5)
+    //     ctx.moveTo(0, 400/2)
+    //     ctx.lineTo(500, 400/2)
+    //     ctx.stroke()
+
+    //     const k = 50
+
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = 'red'
+    //     ctx.stroke()
+    //     ctx.moveTo(scaleX(xe, k), scaleY(funcDif(xe), k))
+    //     for (let x = xb; x <= xe; x += 0.2) {
+    //         console.log(scaleX(x, k), scaleY(funcDif(x), k))
+    //         ctx.lineTo(scaleX(x, k), scaleY(funcDif(x), k))
+    //     }
+    //     ctx.stroke()
+    //}
